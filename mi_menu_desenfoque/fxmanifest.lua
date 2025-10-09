1f449efeb00eff9be2e56e6d274c664e36699792
Javier143322 <huhdnuhdl@gmail.com>
2025-10-09 17:57:11 -0500

    Actualizar fxmanifest.lua

--- mi_menu_desenfoque/fxmanifest.lua
+++ mi_menu_desenfoque/fxmanifest.lua
@@ -1,1 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+-- 1. Indica la página web principal
+ui_page 'html/index.html'
+
+-- 2. Lista los archivos que el navegador va a usar
+files {
+    'html/index.html',
+    'html/app.js',
+}
 
